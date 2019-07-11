f56760d610de66a1f69714a2f103bebc544cfba1
cdra0624 <t.adaniya@adawarp.com>
2019-07-11 15:08:07 +0900

    chore: add index to user.email

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_10_210309) do
+ActiveRecord::Schema.define(version: 2019_07_10_221540) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,11 +21,20 @@ ActiveRecord::Schema.define(version: 2019_07_10_210309) do
     t.string "author", limit: 255, null: false
   end
 
+  create_table "tigers", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.integer "age"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
 end
