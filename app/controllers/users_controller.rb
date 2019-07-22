4b0427a59d126a81efa1f1fd6579122892d138de
cdra0624 <t.adaniya@adawarp.com>
2019-07-22 15:31:32 +0900

    feat: implement user delete by admin

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
-  before_action :logged_in_user, only: [:edit, :update]
+  before_action :logged_in_user, only: [:index, :edit, :update, :destroy]
   before_action :correct_user, only: [:show, :edit, :update]
+  before_action :admin_user, only: :destroy
 
   def index
     @users = User.paginate(page: params[:page], per_page: 2)
@@ -40,6 +41,12 @@ class UsersController < ApplicationController
     end
   end
 
+  def destroy
+    User.find(params[:id]).destroy
+    flash[:success] = "User deleted"
+    redirect_to users_url
+  end
+
   private
     def user_params
       params.require(:user).permit(:name, :email, :password, :password_confirmation)
@@ -56,6 +63,13 @@ class UsersController < ApplicationController
       @user = User.find(params[:id])
       if !current_user?(@user)
         flash[:danger] = "not allowed to access"
+        redirect_to root_url
+      end
+    end
+
+    def admin_user
+      if !current_user.admin?
+        flash[:danger] = "This action is not allowed"
         redirect_to root_url
       end
     end
