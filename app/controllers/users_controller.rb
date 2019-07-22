2d26336e50b8d4850410dbe193a4e325855177a5
cdra0624 <t.adaniya@adawarp.com>
2019-07-22 19:18:53 +0900

    feat: add before action to validate non-admin user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -42,7 +42,7 @@ class UsersController < ApplicationController
   end
 
   def destroy
-    User.find(params[:id]).destroy
+    # User.find(params[:id]).destroy
     flash[:success] = "User deleted"
     redirect_to users_url
   end
