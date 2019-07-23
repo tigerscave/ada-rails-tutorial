73d6e9fd4db59d206ac6b978870cb55812650fcf
cdra0624 <t.adaniya@adawarp.com>
2019-07-23 11:53:09 +0900

    feat(model/user): add activation_token variable

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
-  attr_accessor :remember_token
+  attr_accessor :remember_token, :activation_token
+  before_create :create_activation_digest
 
   validates(:name, presence: true)
   validates :email, presence: true, uniqueness: true
@@ -32,4 +33,11 @@ class User < ApplicationRecord
   def forget
     update_attribute(:remember_digest, nil)
   end
+
+  private
+    def create_activation_digest
+      self.activation_token = User.new_token
+      self.activation_digest = User.digest(activation_token)
+    end
+
 end
