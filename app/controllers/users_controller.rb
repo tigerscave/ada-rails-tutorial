d26888f2478bac468ca5c155c791ea4b037bc788
cdra0624 <t.adaniya@adawarp.com>
2019-07-23 17:17:03 +0900

    feat(mailer): enable to activate via mailer

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,7 @@ class UsersController < ApplicationController
   before_action :admin_user, only: :destroy
 
   def index
-    @users = User.paginate(page: params[:page], per_page: 2)
+    @users = User.where(activated: true).paginate(page: params[:page], per_page: 2)
   end
 
   def new
@@ -13,11 +13,15 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+    if !@user.activated
+      redirect_to root_url
+    end
   end
 
   def create
     @user = User.new(user_params)
     if @user.save
+      @user.send_activation_email
       UserMailer.account_activation(@user).deliver_now
       flash[:info] = "Please check your email to activate your account"
       redirect_to root_url
