17da16628f6e38618ac70e5d5257d12e4541d69c
cdra0624 <t.adaniya@adawarp.com>
2019-07-23 16:07:23 +0900

    feat(mailer): enable to preview

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -10,11 +10,6 @@ class UserMailer < ApplicationMailer
   #
   #   en.user_mailer.account_activation.subject
   #
-  def account_activation
-    @greeting = "Hi"
-
-    mail to: "to@example.org"
-  end
 
   # Subject can be set in your I18n file at config/locales/en.yml
   # with the following lookup:
