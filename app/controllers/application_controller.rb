5cda4cd8468a024bcf2c7b0b763ce01d3426d90b
cdra0624 <t.adaniya@adawarp.com>
2019-07-24 13:28:19 +0900

    feat(micropost): implement form to home

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,12 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   include SessionsHelper
+
+  private
+    def logged_in_user
+      if !logged_in?
+        flash[:danger] = "Please log in."
+        redirect_to login_url
+      end
+    end
 end
