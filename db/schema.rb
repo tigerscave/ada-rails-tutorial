00a505703beb614facde6e8b35da5f2eef72eb76
cdra0624 <t.adaniya@adawarp.com>
2019-07-24 12:17:00 +0900

    feat(migration): add index to micropost user_id and created_at

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_23_202751) do
+ActiveRecord::Schema.define(version: 2019_07_24_031049) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,6 +19,15 @@ ActiveRecord::Schema.define(version: 2019_07_23_202751) do
     t.string "title", limit: 255, null: false
     t.text "description", null: false
     t.string "author", limit: 255, null: false
+  end
+
+  create_table "microposts", force: :cascade do |t|
+    t.text "content"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id", "created_at"], name: "index_microposts_on_user_id_and_created_at"
+    t.index ["user_id"], name: "index_microposts_on_user_id"
   end
 
   create_table "tigers", force: :cascade do |t|
@@ -45,4 +54,5 @@ ActiveRecord::Schema.define(version: 2019_07_23_202751) do
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
+  add_foreign_key "microposts", "users"
 end
