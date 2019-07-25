56a32afc42e1abcb15bebc923c2954341588d6a2
cdra0624 <t.adaniya@adawarp.com>
2019-07-25 09:44:39 +0900

    feat: add picture field to micropost model

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,5 +1,6 @@
 class Micropost < ApplicationRecord
   belongs_to :user
+  mount_uploader :picture, PictureUploader
   validates :user_id, presence: true
   validates :content, presence: true, length: { maximum: 140 }
   default_scope -> { order(created_at: :desc) }
